b8690ed229785dd3915448fda0851135907fb87e
karolllus <cardobosz@gmail.com>
2021-02-27 15:53:29 +0100

    django_debug

--- geoip_project/settings.py
+++ geoip_project/settings.py
@@ -28,9 +28,10 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['localhost', '127.0.0.1']
 
 
 # Application definition
