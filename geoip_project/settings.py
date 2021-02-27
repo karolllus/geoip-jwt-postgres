7368ce705e9359cccdb2d95b7afb11406cd45e60
karolllus <cardobosz@gmail.com>
2021-02-28 00:14:06 +0100

    ALLOWED_HOSTS change

--- geoip_project/settings.py
+++ geoip_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS =  ['localhost', '127.0.0.1']
+ALLOWED_HOSTS =  ['*']
 
 # Application definition
 
